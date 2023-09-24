5e3ad0bd00d5ad0482651c3bc27dfffa5b06eeba
Souata Majumdar <majumdar.deba@gmail.com>
2023-09-24 11:28:02 +0530

    Commiting the changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import LoginForm from './LoginForm'
 import './App.css';
+import Parent from './Parent';
 
 function App() {
   return (
-    <div className="app">
-      <h1>TDD Basics with React</h1>
-      <LoginForm submit={values => alert(JSON.stringify(values))}/>
+    // <div className="app">
+    //   <h1>TDD Basics with React</h1>
+    //   <LoginForm submit={values => alert(JSON.stringify(values))}/>
+    // </div>
+    <div>
+      <Parent/>
     </div>
   );
 }
